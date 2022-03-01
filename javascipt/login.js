94e88346590232de6a7d825044aa9c32d3f545d3
Naman <goyalnaman@outlook.com>
2022-03-01 19:23:36 +0530

    added basic validation for user login

--- javascipt/login.js
+++ javascipt/login.js
@@ -2,6 +2,18 @@ $("form").on("submit", function (event) {
   event.preventDefault();
   const email = $("input[id=email]").val();
   const password = $("input[id=password]").val();
-  console.log(email);
-  console.log(password);
+  $.getJSON("/resources/login.json", function (users) {
+    const user = users.filter(users => email === users.email && password === users.password);
+    console.log(user);
+    if(user.length)
+    {
+      $("#errormessage").addClass("d-none");
+      console.log("logged in");
+    }
+    else
+    {
+      $("#errormessage").removeClass("d-none");
+      console.log("error in username or pass");
+    }
+  });
 });
